4eddf21da0582f15dedcd8b626f5faac82f827e8
hftamayo <hftamayo@gmail.com>
2021-08-26 20:39:55 -0600

    updating lesson6 exercises

---
+++ phase1/lesson6/exercise23/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gaussianhtamayo',
+    version='0.1',
+    packages=['distributions'],
+    url='http://www.hftamayo.com',
+    license='MIT',
+    author='hftamayo',
+    author_email='hftamayo@gmail.com',
+    description='Gaussiand distribution ML with AWS',
+    zip_safe=False
+)
