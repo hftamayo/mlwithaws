10379efd06b137b7fda5e7a1fd366bbe3e921bd7
hftamayo <hftamayo@gmail.com>
2021-08-30 21:25:09 -0600

    working on exercise 23, bug on execution

---
+++ phase1/lesson6/exercise23/distributions/__init__.py
@@ -0,0 +1,1 @@
+from .gaussiandistribution import read_data_file, calculate_mean, calculate_stdev, plot_histogram, pdf, plot_histogram_pdf, __add__, __repr__
---
+++ phase1/lesson6/exercise23a/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='niggazz',
+    version='0.1',
+    packages=['niggazz'],
+    url='http://www.hftamayo.com',
+    license='MIT',
+    author='hftamayo',
+    author_email='hftamayo@gmail.com',
+    description='niggazz functions',
+    zip_safe=False
+)
